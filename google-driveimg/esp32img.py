46001904542cd6f36b9bd5ca8c20175b9ae7e068
Shivam Sharma <sharmashivam66151@gmail.com>
2022-02-20 15:54:17 +0000

    minor changes

--- google-driveimg/esp32img.py
+++ google-driveimg/esp32img.py
@@ -1,8 +1,8 @@
 # Required Libraries
 import cv2
-import numpy as np  # TO convert images into array
+import numpy as np  # To convert images into array
 import urllib.request # for Opening and reading url
-import time
+import time 
 from pydrive.drive import GoogleDrive
 from pydrive.auth import GoogleAuth
 
@@ -21,3 +21,10 @@ fold_id = "1vvAzTtx4RQ_mv6Pn3uZh3kEcFgnfUDrX"
 while True:
     img_response = urllib.request.urlopen(cam_url)   # Opening and reading url
     imgnp = np.array(bytearray(img_response.read()) , dtype = np.uint8)
+    frame = cv2.imdecode(imgnp, -1)
+
+    cv2.imshow("live Streaming",frame)
+
+    key = cv2.waitKey(5)
+
+    if key == 
